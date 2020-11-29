7fc55b6f6bcd9d0c3ace87e79214d40c7d9f64e6
Lapd1103 <74575333+Lapd1103@users.noreply.github.com>
2020-11-28 21:25:43 -0500

    Gasolina
    lo mismo

--- RUN.py
+++ RUN.py
@@ -1,5 +1,5 @@
 # Proyecto RUN
-# Pygame [ https://www.pygame.org/docs/ ]
+# Pygame  [ https://www.pygame.org/docs/ ] 
 
 import pygame
 import sys
@@ -13,6 +13,7 @@ color_rojo = (255,0,0)
 color_negro = (0,0,0)
 color_azul = (0,0,255)
 color_verde = (0,255,0)
+color_blanco = (255,255,255)
 
 #Carriles 
 carril_1=250
@@ -55,18 +56,21 @@ def detectar_colision(jugador_pos,enemigo_pos):
 		return False
 
 def bGasolina(gasolina):
-	x=0
-	y=0
+	x=50
+	y=9.5
+	pygame.draw.rect(ventana, color_blanco,(47,6.5,106,26),3)
 	pygame.draw.rect(ventana, color_verde, (x, y, gasolina, 20))	
 
-def recarga_gasolina(jugador_pos,galon_pos):
+def recarga_gasolina(jugador_pos):
+	global galon_pos
 	jx = jugador_pos[0]
 	jy = jugador_pos[1]
 	gx = galon_pos[0]
 	gy = galon_pos[1]
 
 	if (gx >= jx and gx <(jx + jugador_size)) or (jx >= gx and jx < (gx + galon_size)):
-		if (gy >= jy and gy <(jy + jugador_size)) or (jy >= gy and jy < (gy + galon_size)):
+		if (gy >= jy and gy <(jy + jugador_size)) or (jy >= gy and jy < (gy + galon_size)): 
+			galon_pos[1]=ALTO
 			return True
 		return False
 
@@ -100,16 +104,19 @@ while not game_over:
 		game_over = True
 
 	#Desplazamiento galon de gasolina
-	if galon_pos[1] >= 0 and galon_pos[1] < ALTO:
-		galon_pos[1] += 5
+	if galon_pos[1] >= 0 and galon_pos[1] < 2000:
+		galon_pos[1] += 7
 	else:
 		galon_pos[0] = random.randint(0,ANCHO - galon_size)
 		galon_pos[1] = 0
 
 	#Recarga de gasolina
 
-	if recarga_gasolina(jugador_pos,galon_pos):
-		gasolina+=5	
+	if recarga_gasolina(jugador_pos):
+		if gasolina<=90:
+			gasolina+=10
+		else: 
+			gasolina+=100-gasolina		
 
 	#Dibujar enemigo
 	pygame.draw.rect(ventana, color_azul ,(enemigo_pos[0],enemigo_pos[1],enemigo_size, enemigo_size))
@@ -120,17 +127,22 @@ while not game_over:
 			jugador_size,jugador_size))
 
 	#Dibujar galon gasolina
-	pygame.draw.rect(ventana, color_verde,
+	gas = pygame.image.load('Pixel_Art/Gas.png')
+	ventana.blit(gas,(galon_pos[0],galon_pos[1]))
+
+	"""pygame.draw.rect(ventana, color_verde,
 			(galon_pos[0],galon_pos[1],
-			galon_size, galon_size))
+			galon_size, galon_size))"""
 
 		#Dibujar enemigo
 	pygame.draw.rect(ventana, color_azul,
 			(enemigo_pos[0],enemigo_pos[1],
 			enemigo_size, enemigo_size))
-	
-	
+
+
+	icono_gas = pygame.image.load('Pixel_Art/Gas_icono.png')
+	ventana.blit(icono_gas,(10,5))	
 	bGasolina(gasolina)	
-	gasolina-=0.08
+	gasolina-=0.04
 	clock.tick(30)
 	pygame.display.update()
